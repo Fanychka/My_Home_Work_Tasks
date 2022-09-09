f87e38694c910441e5b8d9271c189eb4647e67f5
PipBoy <pk7@ya.ru>
2022-09-09 14:19:41 +0700

    Task08_done

---
+++ Task08/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+
+string input_number = Console.ReadLine();
+int Number = int.Parse(input_number);
+
+//int number = 5;
+
+for (int i = 1; i < Number; i++)
+if (i % 2 == 0)
+
+Console.WriteLine($"{i} - четное число входящее в число {Number}");
+
