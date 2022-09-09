6622767e67004c4fc3a230643da272a84a8705ed
PipBoy <pk7@ya.ru>
2022-09-09 12:36:16 +0700

    Task06_done

---
+++ Task06/Program.cs
@@ -0,0 +1,13 @@
+﻿ Console.WriteLine("Введите число: ");
+ 
+        string input_number = Console.ReadLine();
+        int Number = int.Parse(input_number);
+
+        
+        
+        
+        if (Number % 2 == 0) 
+            Console.WriteLine($"Введенное число {Number} является четным ");
+
+        if (Number % 2 != 0)
+            Console.WriteLine($"Введенное число {Number} не является четным");
