eee9c7a92ec849fdc791c078be5e2b5901a705b3
PipBoy <pk7@ya.ru>
2022-09-12 01:44:07 +0700

    CorrectedFromTecher

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -1,9 +1,14 @@
-﻿Console.WriteLine("Введите число: ");
+﻿// Задача 8: 
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
-string input_number = Console.ReadLine();
-int Number = int.Parse(input_number);
+Console.WriteLine("Введите число: ");
 
-for (int i = 1; i < Number; i++)
+//string inputNumber = Console.ReadLine(); 
+
+
+int Number = int.Parse(Console.ReadLine()); // Парсить можно одновременно с чтением входных данных: //int num = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= Number; i++)
 if (i % 2 == 0)
 
 Console.WriteLine($"{i} - четное число входящее в число {Number}");
