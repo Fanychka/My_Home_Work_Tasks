4581001b2a7b16d44da627a37516fab2b0607ae4
PipBoy <pk7@ya.ru>
2022-09-12 01:55:30 +0700

    CorrectedWithTeacherComment

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,19 +1,25 @@
-﻿        Console.WriteLine("Введите первое число: ");
+﻿        // Задача 2: 
+        // Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.       
+        
 
-        string first_input = Console.ReadLine();
 
-        int first_number = int.Parse(first_input);
+        Console.WriteLine("Введите первое число: ");
+
+                                                                                // string first_input = Console.ReadLine();
+
+        int first_number = int.Parse(Console.ReadLine());
 
         Console.WriteLine("Введите второе число: ");
 
-        string second_input = Console.ReadLine();
+                                                                                // string second_input = Console.ReadLine();
         
-        int second_number = int.Parse(second_input);
+        int second_number = int.Parse(Console.ReadLine());
 
         int maxNumber = second_number; // "присваиваем" максимальное значение любому вводимому числу (метод сравнения гирек) 
         int minNumber;
         
-        if (second_number < first_number) maxNumber = first_number; minNumber = second_number;
-        if (second_number > first_number) minNumber = first_number;
+        if (second_number < first_number) {
+                maxNumber = first_number; minNumber = second_number;}
+        else { minNumber = first_number;}
     
         Console.WriteLine($"Большее число здесь {maxNumber}, а меньшее {minNumber}");
